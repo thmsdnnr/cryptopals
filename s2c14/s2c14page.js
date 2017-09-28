e51101c28ab966bef7dc2117474d36e63ad152e8
thmsdnnr <thmsdnnr@gmail.com>
2017-09-28 09:19:21 -0500

    s2c14

---
+++ s2c14/s2c14page.js
@@ -0,0 +1,10 @@
+window.onload = function () {
+  let res=document.querySelector('div#result');
+  var worker = new Worker('decryptTask.js');
+  worker.addEventListener('message', function(e) {
+    e.data.res=e.data.res.replace(/\n/g,'<br />');
+    if (e.data.type=='intermediate') { res.innerHTML=e.data.res; }
+    else { res.innerHTML=`<span style="color:#006F6B">${e.data.res.trim()}</span>`; }
+  });
+  worker.postMessage('gogogo');
+};
